75370adc43924e1f847deb0981549334b36670c3
d1Rocky <edenyovan@gmail.com>
2024-12-21 17:11:02 -0600

    range, slice and working with odd/even numbers

--- assignment2.py
+++ assignment2.py
@@ -5,3 +5,7 @@ for value in range(1,5):
     squares.append(value**2) #calculate the square of the number and add it to the list
 
 print(squares)
+
+#advanced option is to do this:
+squares = [value**2 for value in range(1,11)]
+print(squares)
